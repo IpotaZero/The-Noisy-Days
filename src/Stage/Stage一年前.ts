5bfa77f39ea31bd8e4dbb356705991b6e106f9ae
ipota <kiratoki512810@gmail.com>
2026-03-20 00:30:59 +0900

    不要なコメントを削除

--- src/Stage/Stage一年前.ts
+++ src/Stage/Stage一年前.ts
@@ -4,12 +4,8 @@ export default class extends Stage {
     protected *G(): Generator<void, void, unknown> {
         yield* this.wait(60)
 
-        // 後で人名を表示する機能を作る
-
-        // シオン
         yield* this.text("『あの……これ……。』", { name: "シオン" })
 
-        // レイ
         yield* this.text("『……。』", { name: "レイ" })
         yield* this.text("『何? これ。』", { name: "レイ" })
 
