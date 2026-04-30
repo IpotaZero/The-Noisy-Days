2391edad53011459cb1280a4c68daf37b3d17a71
ipota <kiratoki512810@gmail.com>
2026-04-30 15:19:45 +0900

    テキスト修正

--- src/Stage/StageDust.ts
+++ src/Stage/StageDust.ts
@@ -6,34 +6,20 @@ export default class extends Stage {
 
         yield* this.text("「あなた、合成人でしょう。」", { name: "サカイ" })
         yield* this.text("「誰だアンタは。」", { name: "シオン" })
-        yield* this.text("「トウキョウ警察機動隊アルファのサカイです。」", {
-            name: "サカイ",
-        })
+        yield* this.text("「トウキョウ警察機動隊アルファのサカイです。」", { name: "サカイ" })
         yield* this.text("「……シオン・シマ。」", { name: "シオン" })
-        yield* this.text("「それで、あなたしかも、子供でしょう。」", {
-            name: "サカイ",
-        })
-        yield* this.text("「ハタチを大人とするなら、そう。」", {
-            name: "シオン",
-        })
+        yield* this.text("「それで、あなたしかも、子供でしょう。」", { name: "サカイ" })
+        yield* this.text("「ハタチを大人とするなら、そう。」", { name: "シオン" })
         yield* this.text("「可哀相に。」", { name: "サカイ" })
         yield* this.text("「……。」", { name: "シオン" })
-        yield* this.text("「SILOの中に居れば、傷付く事なんて無かったのに。」", {
+        yield* this.text("「SILOの中に居れば、傷付く事なんて無かったのに。」", { name: "サカイ" })
+        yield* this.text("「その代わりに絶滅しろと?」", { name: "シオン" })
+        yield* this.text("「そんなこと言ってないでしょう!? あなた、TAMAMUSHIの洗脳を受けてるのね。」", {
             name: "サカイ",
         })
-        yield* this.text("「その代わりに絶滅しろと?」", { name: "シオン" })
-        yield* this.text(
-            "「そんなこと言ってないでしょう!? あなた、TAMAMUSHIの洗脳を受けてるのね。」",
-            { name: "サカイ" },
-        )
         yield* this.text("「可哀相に!」", { name: "サカイ" })
-        yield* this.text("「本当に、100%の善意で、そんなことを。」", {
-            name: "シオン",
-        })
-        yield* this.text(
-            "「そうよ! 全て、あなたたちの為にやってるんじゃない!」",
-            { name: "サカイ" },
-        )
+        yield* this.text("「本当に、100%の善意で、そんなことを。」", { name: "シオン" })
+        yield* this.text("「そうよ! 全て、あなたたちの為にやってるんじゃない!」", { name: "サカイ" })
         // 本当に分かり合えない人がいるという絶望
         yield* this.text("「……ッ!!」", { name: "シオン" })
         yield* this.text("「もう、やるしかないのよっ!」", { name: "サカイ" })
@@ -43,8 +29,6 @@ export default class extends Stage {
         yield* this.wait(30)
 
         yield* this.text("「……はは、はははっ!」", { name: "シオン" })
-        yield* this.text("「善意が、傍から見ればこんなにも滑稽だなんて!」", {
-            name: "シオン",
-        })
+        yield* this.text("「善意が、傍から見ればこんなにも滑稽だなんて!」", { name: "シオン" })
     }
 }
