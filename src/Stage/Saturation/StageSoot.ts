9be1c7a17d0e38cf028a2c15baf8c91c845d61b8
ipota <kiratoki512810@gmail.com>
2026-05-02 00:56:06 +0900

    テキスト修正

--- src/Stage/Saturation/StageSoot.ts
+++ src/Stage/Saturation/StageSoot.ts
@@ -32,7 +32,7 @@ export default class extends Stage {
         yield* this.text("「生憎あたしはロボットじゃあない。」", { name: "シオン" })
         yield* this.text("「って、にゃるへそ『当たらなければどうということはない』<br>……ってかぁ?」", { name: "イシカワ" })
         yield* this.text("「てゆーか、なんで君は戦ってるのかな?」", { name: "イシカワ" })
-        yield* this.text("「SILOは悪意を増幅させる!」", { name: "シオン" })
+        yield* this.text("「SILOは悪意を増幅させる! 第二の機械大戦を起こす訳にはいかない!」", { name: "シオン" })
         yield* this.text("「それは建前だ。本当は誰も悪意なんて持ってない事、分かってんでしょう?」", { name: "イシカワ" })
         yield* this.text("「悪意があるから人が死ぬんだろっ!?」", { name: "シオン" })
         yield* this.text("「それにはハンロンさせてもらうよっ!」", { name: "イシカワ" })
